63af2eeba675d327e59e998a76649f0517c9d106
Ananya Phadke <phadkeananya@gmail.com>
2018-07-11 16:07:48 +0000

    automatically backed up by learn

---
+++ iteration_challenges.rb
@@ -0,0 +1,23 @@
+cart_item_prices = [12.43, 19.99, 3.49, 75.00]
+count = 1
+cart_item_prices.each do |price|
+  puts "Item #{ count}: #{price}"
+  count += 1
+end
+
+final_price = []
+count = 1
+cart_item_prices.each do |final|
+  final_price[count] = (final*1.17).round(2)
+  puts "Item #{count}: #{final_price[count]}" 
+  count += 1
+end
+
+expensive_array = [ ]
+count = 1
+cart_item_prices.each do |expensive|
+  if expensive > 15
+    expensive_array << expensive
+  else
+end
+end
