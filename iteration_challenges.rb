731d4616b6141a0224ea582001829cb923aec839
Ananya Phadke <phadkeananya@gmail.com>
2018-07-11 16:13:33 +0000

    automatically backed up by learn

--- iteration_challenges.rb
+++ iteration_challenges.rb
@@ -18,24 +18,10 @@ count = 1
 cart_item_prices.each do |expensive|
   if expensive > 15
     expensive_array << expensive
+    
   else
 end
 
-puts cart_item_prices = [12.43, 19.99, 3.49, 75.00]
-count = 1
-cart_item_prices.each do |price|
-  puts "Item #{ count}: #{price}"
-  count += 1
-end
-
-final_price = []
-count = 0
-cart_item_prices.each do |final|
-  final_price[count] = (final*1.17).round(2)
-  puts "Item #{count+1}: #{final_price[count]}" 
-  count += 1
-end
-
-puts expensive_array
+puts "Item  #{expensive_array}" 
 
 end
